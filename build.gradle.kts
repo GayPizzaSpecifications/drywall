0e0d0158ae9ed9edfc2ae1f98b5e08e90ae1fdbb
a dinosaur <dino@a-dinosaur.com>
2023-07-03 16:04:22 +1000

    maven publishing workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	`kotlin-dsl`
 	kotlin("plugin.serialization") version "1.5.31"
+	`maven-publish`
 }
 
 group = "gay.pizza.foundation"
@@ -36,4 +37,42 @@ gradlePlugin {
 			implementationClass = "${id}.ResourcesGeneratorPlugin"
 		}
 	}
+}
+
+publishing {
+  repositories {
+    mavenLocal()
+
+    var githubPackagesToken = System.getenv("GITHUB_TOKEN")
+    if (githubPackagesToken == null) {
+      githubPackagesToken = project.findProperty("github.token") as String?
+    }
+
+    var gitlabPackagesToken = System.getenv("GITLAB_TOKEN")
+    if (gitlabPackagesToken == null) {
+      gitlabPackagesToken = project.findProperty("gitlab.com.accessToken") as String?
+    }
+
+    maven {
+      name = "GitHubPackages"
+      url = uri("https://maven.pkg.github.com/gaypizzaspecifications/drywall")
+      credentials {
+        username = project.findProperty("github.username") as String? ?: "gaypizzaspecifications"
+        password = githubPackagesToken
+      }
+    }
+
+    maven {
+      name = "GitLab"
+      url = uri("https://gitlab.com/api/v4/projects/47347654/packages/maven")
+      credentials(HttpHeaderCredentials::class.java) {
+        name = "Private-Token"
+        value = gitlabPackagesToken
+      }
+
+      authentication {
+        create<HttpHeaderAuthentication>("header")
+      }
+    }
+  }
 }
